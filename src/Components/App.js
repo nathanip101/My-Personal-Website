e9a3573bc852bbc156246b2524ac21a2772871b7
nathanip <ipfamilynip@gmail.com>
2023-01-10 22:58:43 -0800

    Site Coming Soon

---
+++ src/Components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Site Coming Soon
+        </p>
+      </header>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/data.js
@@ -0,0 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin , faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
+
+export const socialLinks = [
+    {label: 'Github', Icon: faGithub, href: 'https://github.com/nathanip101'},
+    {label: 'Linkedin', Icon: faLinkedin, href: 'https://linkedin.com/in/nathanip101'},
+    {label: 'Instagram', Icon: faInstagram, href: 'https://instagram.com/nahtan.photography'}
+  ];
+  
